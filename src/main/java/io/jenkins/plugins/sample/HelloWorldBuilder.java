dc5fbaddec3f49cd358c00a29df6cff689970d2e
mengyanghan <1347608365@qq.com>
2018-10-31 15:22:07 +0800

    update

--- src/main/java/io/jenkins/plugins/sample/HelloWorldBuilder.java
+++ src/main/java/io/jenkins/plugins/sample/HelloWorldBuilder.java
@@ -1,8 +1,18 @@
 package io.jenkins.plugins.sample;
-
+import hidden.jth.org.apache.http.HttpEntity;
+import hidden.jth.org.apache.http.NameValuePair;
+import hidden.jth.org.apache.http.client.entity.UrlEncodedFormEntity;
+import hidden.jth.org.apache.http.client.methods.CloseableHttpResponse;
+import hidden.jth.org.apache.http.client.methods.HttpPost;
+import hidden.jth.org.apache.http.impl.client.CloseableHttpClient;
+import hidden.jth.org.apache.http.impl.client.HttpClients;
+import hidden.jth.org.apache.http.message.BasicNameValuePair;
+import hidden.jth.org.apache.http.util.EntityUtils;
+import hudson.EnvVars;
 import hudson.Launcher;
 import hudson.Extension;
 import hudson.FilePath;
+import hudson.model.Result;
 import hudson.util.FormValidation;
 import hudson.model.AbstractProject;
 import hudson.model.Run;
@@ -17,12 +27,11 @@ import javax.servlet.ServletException;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Vector;
 
 import jenkins.tasks.SimpleBuildStep;
 import org.jenkinsci.Symbol;
@@ -30,7 +39,6 @@ import org.kohsuke.stapler.DataBoundSetter;
 import org.kohsuke.stapler.StaplerRequest;
 import org.w3c.dom.*;
 import org.xml.sax.SAXException;
-
 public class HelloWorldBuilder extends Builder implements SimpleBuildStep {
 
     private final String reportPath;
@@ -58,9 +66,19 @@ public class HelloWorldBuilder extends Builder implements SimpleBuildStep {
         boolean useProxy = getDescriptor().getUseProxy();
         String proxyUrl = getDescriptor().getProxyUrl();
         String m_dominoPmd = getDescriptor().getDominoPmd();
+
+        EnvVars env = null;
+        String jobName = "";
+        String buildNumber = "";
+        if(env != null){
+            jobName = env.get("JOB_NAME");
+            buildNumber = env.get("BUILD_NUMBER");
+        }
+
+
         command = "cmd /c e: & cd "+m_dominoPmd+ "& pmd -d "+projectPath + reportPath + pmdReportName + pmdCMD;
         listener.getLogger().println("PMDcmd:"+ pmdCMD);
-        listener.getLogger().println("m_ReportPath:"+ notesAddr);
+        listener.getLogger().println("m_ReportPath:"+ reportPath);
         listener.getLogger().println("m_PmdCmd:"+ reportPath);
         listener.getLogger().println("m_dominoServer:"+m_dominoServer);
         listener.getLogger().println("m_dominoMailbox:"+m_dominoMailbox);
@@ -80,7 +98,104 @@ public class HelloWorldBuilder extends Builder implements SimpleBuildStep {
                 unitTestReport.add(listOfFiles[i]);
             }
         }
+        String content = this.parsePMDReports(unitTestReport);
+        listener.getLogger().println(content);
+        String subject = jobName+"单元测试报告(#"+buildNumber+")";
+        String mailbody = content;
 
+        if(!useProxy){
+            if(notesAddr != null && !"".equals(notesAddr)){
+                listener.getLogger().println("直接访问Domino服务器发邮件");
+                listener.getLogger().println("m_dominoServer:"+m_dominoServer);
+                listener.getLogger().println("m_dominoMailbox:"+m_dominoMailbox);
+                listener.getLogger().println("m_dominoUsername:"+m_dominoUsername);
+                listener.getLogger().println("m_dominoPassword:"+m_dominoPassword);
+                listener.getLogger().println("pmd命令：" + this.pmdCMD);
+                listener.getLogger().println("wojiushishia："+this.reportPath);
+                listener.getLogger().println("notesishia："+this.notesAddr);
+                NotesSender notesSender = new NotesSender(m_dominoServer,m_dominoMailbox,m_dominoUsername,m_dominoPassword);
+                Vector<String> v_n_sendTo = new Vector<String>();
+                Vector<String>  v_n_copyTo = new Vector<String>();
+                Vector<String>  v_m_strZipFileName = new Vector<String>();
+                v_n_copyTo.clear();
+                v_n_sendTo.clear();
+
+                String[] notes = notesAddr.split(",");
+                for(String note : notes){
+                    v_n_sendTo.add(note);
+                }
+
+                try {
+                    notesSender.send(v_n_sendTo, v_n_copyTo, null, subject, mailbody, v_m_strZipFileName);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    listener.getLogger().println("构建失败:发送NOTES邮件失败");
+                    build.setResult(Result.FAILURE);
+                    return;
+                }
+            }else{
+                listener.getLogger().println("未配置NOTES地址，不发送邮件");
+            }
+        }else{
+            if(notesAddr != null && !"".equals(notesAddr)){
+                listener.getLogger().println("使用代理发NOTES");
+                listener.getLogger().println("m_dominoServer:"+m_dominoServer);
+                listener.getLogger().println("m_dominoMailbox:"+m_dominoMailbox);
+                listener.getLogger().println("m_dominoUsername:"+m_dominoUsername);
+                listener.getLogger().println("m_dominoPassword:"+m_dominoPassword);
+                listener.getLogger().println("receivers:"+notesAddr);
+                String fullUrl = "http://"+proxyUrl+"/send";
+                listener.getLogger().println("代理地址:"+fullUrl);
+                CloseableHttpClient closeableHttpClient = HttpClients.createDefault();
+                HttpPost httpost = new HttpPost(fullUrl);
+                List<NameValuePair> paramList = new ArrayList<NameValuePair>();
+                paramList.add(new BasicNameValuePair("dominoServer", m_dominoServer));
+                paramList.add(new BasicNameValuePair("dominoMailbox", m_dominoMailbox));
+                paramList.add(new BasicNameValuePair("dominoUsername", m_dominoUsername));
+                paramList.add(new BasicNameValuePair("dominoPassword", m_dominoPassword));
+                paramList.add(new BasicNameValuePair("receivers", notesAddr));
+                paramList.add(new BasicNameValuePair("subject", subject));
+                paramList.add(new BasicNameValuePair("mailbody", mailbody));
+
+                try {
+                    httpost.setEntity(new UrlEncodedFormEntity(paramList, "UTF-8"));
+                } catch (UnsupportedEncodingException e1) {
+                    e1.printStackTrace();
+                }
+
+                String result = "";
+                CloseableHttpResponse httpResponse = null;
+                try {
+                    httpResponse = closeableHttpClient.execute(httpost);
+                    HttpEntity entity = httpResponse.getEntity();
+                    result = EntityUtils.toString(entity, "UTF-8");
+                    listener.getLogger().println("代理返回结果为:"+result);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    build.setResult(Result.FAILURE);
+                    listener.getLogger().println("构建失败:使用代理发送邮件失败,HTTP请求失败");
+                    return;
+                }finally{
+                    try {
+                        httpResponse.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                JSONObject json = JSONObject.fromObject(result);
+                if(!json.getString("retCode").equals("0")){
+                    listener.getLogger().println("构建失败:使用代理发送邮件失败,返回报文解析失败");
+                    build.setResult(Result.FAILURE);
+                    return;
+                }
+            }else{
+                listener.getLogger().println("未配置NOTES地址，不发送邮件");
+            }
+
+        }
+
+        build.setResult(Result.SUCCESS);
 
     }
     @Override
@@ -114,95 +229,73 @@ public class HelloWorldBuilder extends Builder implements SimpleBuildStep {
         }
     }
 
-//    private String parsePMDReports(List<File> reports){
-//        DocumentBuilderFactory a= DocumentBuilderFactory.newInstance();
-//        int priority1 =0;
-//        int priority2 =0;
-//        int priority3 =0;
-//        int priority4 =0;
-//        int priority5 =0;
-//        try{
-//            DocumentBuilder b = a.newDocumentBuilder();
-//            Document document = b.parse("D:\\pmd-report-test.xml");
-//            NodeList booklist1 = document.getElementsByTagName("file");
-//            NodeList booklist = document.getElementsByTagName("violation");
-//            //System.out.println("一共有" + booklist1.getLength() + "个文件检测出问题;" + "一共有" + booklist.getLength() + "个问题.");
-//            System.out.println("============PMD Report=============");
-//            for(int i =0; i<booklist1.getLength(); i++) {
-//                Node book = booklist1.item(i);
-//                NamedNodeMap bookmap = book.getAttributes();
-//            }
-//
-//            for(int i =0; i<booklist.getLength(); i++) {
-//
-//                //循环遍历获取每一个book
-//                Node book = booklist.item(i);
-//                //通过Node对象的getAttributes()方法获取全的属性值
-//                NamedNodeMap bookmap = book.getAttributes();
-//                //循环遍每一个book的属性值
-//                for (int j = 0; j < bookmap.getLength(); j++) {
-//                    Node node = bookmap.item(j);
-//                    //通过Node对象的getNodeName()和getNodeValue()方法获取属性名和属性值
-//                    if (node.getNodeName().equals("priority")&&node.getNodeValue().equals("1")){
-//                        priority1+=1;
-//                    }
-//                    if (node.getNodeName().equals("priority")&&node.getNodeValue().equals("2")){
-//                        priority2+=1;
-//                    }
-//                    if (node.getNodeName().equals("priority")&&node.getNodeValue().equals("3")){
-//                        priority3+=1;
-//                    }
-//                    if (node.getNodeName().equals("priority")&&node.getNodeValue().equals("4")){
-//                        priority4+=1;
-//                    }
-//                    if (node.getNodeName().equals("priority")&&node.getNodeValue().equals("5")){
-//                        priority5+=1;
-//                    }
-//                }
-//                NodeList childlist = book.getChildNodes();
-//            }
-////            String report = "一共有" + booklist1.getLength() + "个文件检测出问题;" + "一共有" + booklist.getLength() + "个问题.\n"+"严重且优先高的错误共有： " + priority1+"个\n"
-////                    + "严重且优先正常的错误共有： " + priority2+"个\n"
-////                    +"严重且优先高的警告共有：" + priority3+"个\n"
-////                    +"严重且优先正常的警告共有： " + priority4+"个\n"
-////                    +"严重且优先正常的信息共有： " + priority5+"个\n"
-////                    +"具体问题请到Jenkins工作流中查询！";
-//            System.out.println("一共有" + booklist1.getLength() + "个文件检测出问题;" + "一共有" + booklist.getLength() + "个问题.");
-//            System.out.println("严重且优先高的错误共有： " + priority1+"个");
-//            System.out.println("严重且优先正常的错误共有： " + priority2+"个");
-//            System.out.println("严重且优先高的警告共有：" + priority3+"个");
-//            System.out.println("严重且优先正常的警告共有： " + priority4+"个");
-//            System.out.println("严重且优先正常的信息共有： " + priority5+"个");
-//            System.out.println("具体问题请到Jenkins工作流中查询！");
-//            for(int i =0; i<booklist1.getLength(); i++) {
-//
-//                //循环遍历获取每一个book
-//                Node book = booklist1.item(i);
-//                //通过Node对象的getAttributes()方法获取全的属性值
-//                NamedNodeMap bookmap = book.getAttributes();
-//                //循环遍每一个book的属性值
-//                for (int j = 0; j < bookmap.getLength(); j++) {
-//                    Node node = bookmap.item(j);
-//                    //System.out.print(node.getNodeName() + ":");
-//                    //System.out.println(node.getNodeValue());
-//                }
-//                //解析book节点的子节点
-//                NodeList childlist = book.getChildNodes();
-//                for (int t = 0; t < childlist.getLength(); t++) {
-//                    //区分出text类型的node以及element类型的node
-//                    if (childlist.item(t).getNodeType() == Node.ELEMENT_NODE) {
-//                        System.out.print(childlist.item(t).getNodeName() + ":"+ childlist.item(t).getTextContent());
-//                        //System.out.println(childlist.item(t).getTextContent());
-//                    }
-//                }
-//            }
-//        }catch (ParserConfigurationException e) {
-//            e.printStackTrace();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//        return "";
-//    }
+    private String parsePMDReports(List<File> reports) {
+        DocumentBuilderFactory a = DocumentBuilderFactory.newInstance();
+        int priority1 = 0;
+        int priority2 = 0;
+        int priority3 = 0;
+        int priority4 = 0;
+        int priority5 = 0;
+        for (File report : reports) {
+            try {
+                DocumentBuilder b = a.newDocumentBuilder();
+                Document document = b.parse(report);
+                NodeList nodeList1 = document.getElementsByTagName("file");
+                NodeList nodeList2 = document.getElementsByTagName("violation");
+                for(int i =0; i<nodeList1.getLength(); i++) {
+                    Node book = nodeList1.item(i);
+                    NamedNodeMap bookmap = book.getAttributes();
+                }
+                for(int i =0; i<nodeList2.getLength(); i++) {
+                    Node node = nodeList2.item(i);
+                    NamedNodeMap nodeMap = node.getAttributes();
+                    for (int j = 0; j < nodeMap.getLength(); j++) {
+                        Node node1 = nodeMap.item(j);
+                        //通过Node对象的getNodeName()和getNodeValue()方法获取属性名和属性值
+                        if (node1.getNodeName().equals("priority")&&node1.getNodeValue().equals("1")){
+                            priority1+=1;
+                        }
+                        if (node1.getNodeName().equals("priority")&&node1.getNodeValue().equals("2")){
+                            priority2+=1;
+                        }
+                        if (node1.getNodeName().equals("priority")&&node1.getNodeValue().equals("3")){
+                            priority3+=1;
+                        }
+                        if (node1.getNodeName().equals("priority")&&node1.getNodeValue().equals("4")){
+                            priority4+=1;
+                        }
+                        if (node1.getNodeName().equals("priority")&&node1.getNodeValue().equals("5")){
+                            priority5+=1;
+                        }
+                        //System.out.print(node.getNodeName() + ":");
+                        //System.out.println(node.getNodeValue());
+                    }
+                }
+                String content = "一共有" + nodeList1.getLength() + "个文件检测出问题;" + "一共有" + nodeList2.getLength() + "个问题.\n"
+                        + "严重且优先高的错误共有： " + priority1+"个\n"
+                        + "严重且优先正常的错误共有： " + priority2+"个\n"
+                        + "严重且优先高的警告共有：" + priority3+"个\n"
+                        + "严重且优先正常的警告共有： " + priority4+"个\n"
+                        + "严重且优先正常的信息共有： " + priority5+"个\n"
+                        + "具体问题请到Jenkins工作流中查询！";
+                System.out.println(content);
+//                System.out.println("一共有" + nodeList1.getLength() + "个文件检测出问题;" + "一共有" + nodeList2.getLength() + "个问题.");
+//                System.out.println("严重且优先高的错误共有： " + priority1+"个");
+//                System.out.println("严重且优先正常的错误共有： " + priority2+"个");
+//                System.out.println("严重且优先高的警告共有：" + priority3+"个");
+//                System.out.println("严重且优先正常的警告共有： " + priority4+"个");
+//                System.out.println("严重且优先正常的信息共有： " + priority5+"个");
+//                System.out.println("具体问题请到Jenkins工作流中查询！");
+            } catch (ParserConfigurationException e) {
+                e.printStackTrace();
+            } catch (SAXException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return "";
+    }
     @Extension
     public static final class DescriptorImpl extends BuildStepDescriptor<Builder> {
         /**
